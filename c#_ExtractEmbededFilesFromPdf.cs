8a7edd50b44d85b975513203baa503b9256561e1
kijato <kijato@gmail.com>
2020-08-28 13:45:31 +0200

    Update c#_ExtractEmbededFilesFromPdf.cs

--- c#_ExtractEmbededFilesFromPdf.cs
+++ c#_ExtractEmbededFilesFromPdf.cs
@@ -4,7 +4,7 @@ wget -nH -nd https://www.nuget.org/api/v2/package/PdfPig/0.1.2
 unzip pdfpig.0.1.2.nupkg
 rename lib pdfpig
 */
-
+// https://web.archive.org/web/20060427192430/http://partners.adobe.com/public/developer/en/pdf/PDFReference16.pdf
 // c:\ProgramData\CS-Script\CSScriptNpp\1.7.24.0\Roslyn\csc.exe
 // csc.exe -r:UglyToad.PdfPig.dll,UglyToad.PdfPig.Tokens.dll -debug- -target:exe -platform:x64 ExtractEmbededFilesFromPdf.cs
 //
@@ -14,6 +14,7 @@ using System.Collections.Generic;
 //using System.Xml;
 using System.Xml.Linq;
 using UglyToad.PdfPig;
+using UglyToad.PdfPig.Core;
 using UglyToad.PdfPig.AcroForms;
 using UglyToad.PdfPig.AcroForms.Fields;
 using UglyToad.PdfPig.Content;
@@ -24,14 +25,31 @@ public static class Program
 {
     public static void Main(string[] args)
     {
-        using (PdfDocument document = PdfDocument.Open(args[0]))
+		//String fn = args[0];
+		String fn = @"c:\Temp\pdfpig\35600_5904-1_2019.pdf";
+        using (PdfDocument document = PdfDocument.Open(fn))
         {
             Console.WriteLine($"Document has {document.NumberOfPages} pages.");
-            //Console.WriteLine("\tCreator: "+document.Information.Creator);
-            //Console.WriteLine("\tAuthor:  "+document.Information.Author);
-            //Console.WriteLine("\tTitle:   "+document.Information.Title);
-            Console.WriteLine("Document properties: "+document.Information.DocumentInformationDictionary);
+            Console.WriteLine("DocumentInformationDictionary: "+document.Information.DocumentInformationDictionary);
+			Console.WriteLine("CalogDictionary: "+document.Structure.Catalog.CatalogDictionary);
+			/*foreach(KeyValuePair<string, IToken> key in document.Structure.Catalog.CatalogDictionary.Data)
+			{
+				Console.WriteLine("\t"+key.Key+" -> "+key.Value+" ( "+key.Value.GetType().ToString()+" )");
+			}*/
 
+			bool hasForm = document.TryGetForm(out AcroForm af);
+			foreach(var key in af.Fields)
+			{
+				Console.WriteLine("\t"+key.Bounds);
+				Console.WriteLine("\t"+key.Dictionary);
+				Console.WriteLine("\t"+key.FieldFlags);
+				Console.WriteLine("\t"+key.FieldType);
+				Console.WriteLine("\t"+key.Information);
+				Console.WriteLine("\t"+key.PageNumber);
+				Console.WriteLine("\t"+key.RawFieldType);
+			}
+			
+			
             if (document.TryGetForm(out AcroForm form))
             {
                 foreach (AcroFieldBase field in form.GetFieldsForPage(1))
@@ -51,7 +69,7 @@ public static class Program
 			{
 				Console.WriteLine($"Document doesn't contains form.");
 			}
-			
+
             if (document.TryGetXmpMetadata(out XmpMetadata metadata))
             {
                 XDocument xmp = metadata.GetXDocument();
@@ -64,7 +82,7 @@ public static class Program
 			{
 				   Console.WriteLine($"Document doesn't contains metadata... :|");
 			}
-            
+
             if (document.TryGetBookmarks(out Bookmarks bookmarks))
             {
                 Console.WriteLine($"Document contained bookmarks with {bookmarks.Roots.Count} root nodes.");
@@ -80,13 +98,13 @@ public static class Program
             if (document.Advanced.TryGetEmbeddedFiles(out IReadOnlyList<EmbeddedFile> embeddedFiles) && embeddedFiles.Count > 0)
             {
                 Console.WriteLine($"Document contains {embeddedFiles.Count} embedded files.");
-				
+
 				foreach(var file in embeddedFiles)
 				{
 					Console.WriteLine("\t"+file);
-					
+
 					IReadOnlyList<byte> bytes = file.Bytes;
-					
+
 					using (BinaryWriter writer = new BinaryWriter(File.Open(args[0]+"_-_"+file.Name, FileMode.Create)))
 					{
 						//writer.Write(bytes);
@@ -96,9 +114,8 @@ public static class Program
 						}
 					}
 					Console.WriteLine("\tThe file saved to: '"+args[0]+"_-_"+file.Name+"'");
-					
+
 				}
-				
             }
         }
     }
