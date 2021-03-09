426a39d092aecf526f52e000bfa6c0905099584f
kijato <kijato@gmail.com>
2021-03-09 16:04:44 +0100

    Update c#_winforms_skeleton_v2.cs.cs

--- c#_winforms_skeleton_v2.cs.cs
+++ c#_winforms_skeleton_v2.cs.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Windows.Forms;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 
 using System.IO;
 using System.Text.RegularExpressions;
@@ -17,12 +18,18 @@ using System.Reflection; // https://codecharm.com/blog/archive/2017-11-19-c-asse
 //[assembly: AssemblyFileVersion("1.0.1.124")]
 
 // https://www.c-sharpcorner.com/article/how-to-draw-shapes-inside-panel-control-in-winforms-using-visual-studio-2017/
-// https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.control.mousehover?view=netframework-4.7.2
+// https://docs.microsoft.com/en-us/dotnet/api/control.mousehover?view=netframework-4.7.2
 
 public class Form1 : Form  
 {  
 
     StatusBar mainStatusBar = new StatusBar();
+    Panel panel1 = new Panel();
+    
+    int x;  
+    int y;
+    
+    GraphicsPath mousePath = new GraphicsPath();
 
     [STAThread]
     public static void Main()
@@ -32,28 +39,86 @@ public class Form1 : Form
         Application.Run(new Form1());
     }   
 
-    protected override void OnPaint(PaintEventArgs pae) 
+    /*protected override void OnPaint(PaintEventArgs pae) 
     { 
       Graphics myGraphics = pae.Graphics; 
       Pen myPen = new Pen(Color.Red, 3); 
       myGraphics.DrawLine(myPen, 30, 20, 300, 100); 
-    }   
+    }  */ 
 
    public Form1()  
    {
         Size = new Size(800, 600);
         Controls.Add(mainStatusBar);
-        MouseMove += new MouseEventHandler(Form1_MouseMove);
-        Paint += new System.Windows.Forms.PaintEventHandler(Form1_Paint);
+        Controls.Add(panel1);
+        panel1.Dock = DockStyle.Fill;
+        panel1.BackColor = Color.White;
+        
+        panel1.Click += new EventHandler(Click);
+        //panel1.MouseLeave += new EventHandler(MouseLeave);
+        //panel1.MouseEnter += new EventHandler(MouseEnter);
+        panel1.MouseHover += new EventHandler(MouseHover);
+
+        panel1.Paint +=      new PaintEventHandler(Paint);
+
+        //panel1.MouseUp +=    new MouseEventHandler(MouseUp);
+        panel1.MouseMove +=  new MouseEventHandler(MouseMove);
+        panel1.MouseDown +=  new MouseEventHandler(MouseDown);
+        //panel1.MouseWheel += new MouseEventHandler(MouseWheel);
+
    }  
 
-   private void Form1_MouseMove(object sender, MouseEventArgs e)  
+    private void MouseHover(object sender, EventArgs e) 
+    {
+        // Update the mouse event label to indicate the MouseHover event occurred.
+        mainStatusBar.Text = sender.GetType().ToString() + ": MouseHover";
+    }   
+   
+   private void MouseDown(object sender, MouseEventArgs e)  
+   {  
+        Point mouseDownLocation = new Point(e.X, e.Y);
+        mousePath.AddLine(mouseDownLocation,mouseDownLocation);
+        panel1.Focus();
+        panel1.Invalidate();
+        
+   } 
+   
+   private void MouseMove(object sender, MouseEventArgs e)  
    {  
         mainStatusBar.Text = e.X + " " + e.Y;
-   }  
-   private void Form1_Paint(object sender, EventArgs e)  
+        
+        switch (e.Button) {
+            case MouseButtons.Left:
+                mainStatusBar.Text += "L";
+                break;
+            case MouseButtons.Middle:
+                mainStatusBar.Text += "M";
+                break;
+            case MouseButtons.Right:
+                mainStatusBar.Text += "R";
+                break;
+            default:
+            break;
+        }
+        
+   } 
+   
+   private void Paint(object sender, PaintEventArgs e)  
    {  
         mainStatusBar.Text = "..";
-   }  
+        Graphics myGraphics = e.Graphics; 
+        Pen myPen = new Pen(Color.Red, 3); 
+        myGraphics.DrawLine(myPen, 30, 20, 300, 100);
+         e.Graphics.DrawPath(System.Drawing.Pens.DarkRed, mousePath);
+   }
+
+    private void Click(object sender, EventArgs e)  
+    {  
+        /*Point p = new Point(e.X, e.Y);  
+        x = p.X;  
+        y = p.Y;  
+        panel1.Invalidate();  */
+        mainStatusBar.Text = "click";
+    }   
  
 }  
